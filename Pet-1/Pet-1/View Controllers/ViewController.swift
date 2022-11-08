1507616ca4124bfd062d87f2a4c6e4cd1b38f06b
Aleksei Alikhanov <aleksei.alikhanov@gmail.com>
2022-11-08 17:19:18 +0300

    added DispatchWorkItem and URLSession

--- Pet-1/Pet-1/View Controllers/ViewController.swift
+++ Pet-1/Pet-1/View Controllers/ViewController.swift
@@ -10,28 +10,39 @@ import UIKit
 class ViewController: UIViewController {
     
     private var imageView: UIImageView!
-    private var image: UIImage!
+    private var imageView2: UIImageView!
+    private var image: [UIImage] = [UIImage(), UIImage()]
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         print("viewdidload")
         loadData()
+//        fetchImage()
+//        fetchImage2()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         initImageView()
+        imageView.image = image[0]
+
     }
     
     private func initImageView(){
         imageView = UIImageView()
-        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-        imageView.center = view.center
+        imageView.frame = CGRect(x: 50, y: 0, width: 150, height: 150)
+        imageView.center.y = view.center.y
         imageView.backgroundColor = .darkGray
-        imageView.image = image
+        imageView.image = image[0]
         view.addSubview(imageView)
+        imageView2 = UIImageView()
+        imageView2.frame = CGRect(x: 250, y: 0, width: 150, height: 150)
+        imageView2.center.y = view.center.y
+        imageView2.backgroundColor = .darkGray
+        imageView2.image = image[1]
+        view.addSubview(imageView2)
     }
-    
+    // classic
     private func loadData(){
         let imageURL = URL(string: "https://i.redd.it/096oc5ubc5381.jpg")!
        let queue = DispatchQueue.global(qos: .utility)
@@ -41,14 +52,46 @@ class ViewController: UIViewController {
                print("load")
                 DispatchQueue.main.async {
                     print("show")
-                    self.image = UIImage(data: data)
-                    self.imageView.image = self.image
+                    self.image[0] = UIImage(data: data)!
+                    self.imageView.image = self.image[0]
                 }
             }
             else {
                 print("error load")
             }
         }
+    }
+    // Used DispatchWorkItem
+    private func fetchImage(){
+        print("DispatchWorkItem start")
+        let imageURL = URL(string: "https://i.redd.it/096oc5ubc5381.jpg")!
+        var data: Data?
+        let queue = DispatchQueue.global(qos: .utility)
+        let worlItem = DispatchWorkItem(qos: .userInteractive) {
+            data = try? Data(contentsOf: imageURL)
+            print(Thread.current)
+        }
+        queue.async(execute: worlItem)
+        worlItem.notify(queue: .main) {
+            if let imageData = data {
+                self.imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    // Used URLSession
+    private func fetchImage2(){
+        print("URLSession started")
+        let imageURL = URL(string: "https://i.redd.it/096oc5ubc5381.jpg")!
+        let task = URLSession.shared.dataTask(with: imageURL) { data, response , error  in
+            print(Thread.current)
+            if let imageData = data {
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: imageData)
+                }
+            }
+        }
+        task.resume()
     }
 }
 
