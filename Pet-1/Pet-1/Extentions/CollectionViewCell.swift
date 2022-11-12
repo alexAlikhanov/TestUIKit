34ce8d00470e18602739a2363a5642ea9fc11d3e
Aleksei Alikhanov <aleksei.alikhanov@gmail.com>
2022-11-12 18:55:44 +0300

    create collectionViewCell in posts

--- Pet-1/Pet-1/Extentions/CollectionViewCell.swift
+++ Pet-1/Pet-1/Extentions/CollectionViewCell.swift
@@ -8,5 +8,57 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
+    let networkManager = NetworkManager()
+    private let loadIndicator = UIActivityIndicatorView()
+    var postData : PostModel!
+    var imageView : UIImageView = {
+       var img = UIImageView()
+        img.layer.cornerRadius = 10
+        img.contentMode = .scaleAspectFill
+        img.layer.masksToBounds = true
+        return img
+    }()
     
+    
+    override init(frame: CGRect) {
+        super .init(frame: CGRect(x: 5, y: 5, width: 100, height: 100))
+        backgroundColor = .white
+        layer.cornerRadius = 10
+        imageView.frame = CGRect(x: 2, y: 2, width: 150, height: 200)
+        contentView.addSubview(imageView)
+        contentView.addSubview(loadIndicator)
+        
+    }
+    func config(){
+        loadIndicator.startAnimating()
+        networkManager.getFirstPhotoInAlbumBy(id: postData.userId) { [weak self] data in
+            DispatchQueue.main.async {
+                self?.imageView.image = UIImage(data: data)
+                self?.loadIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        loadIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+        
+        loadIndicator.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        loadIndicator.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        
+    }
 }
--- Pet-1/Pet-1/Extentions/TableViewCell.swift
+++ Pet-1/Pet-1/Extentions/TableViewCell.swift
@@ -8,17 +8,10 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-    let networkManager = NetworkManager()
     var postData : PostModel!
-    private let loadIndicator = UIActivityIndicatorView()
-    private let myImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.layer.cornerRadius = 12
-        imageView.backgroundColor = .lightGray
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
+    
+    private var collectionView : UICollectionView?
+
     private var userId : UILabel = {
         let userId = UILabel()
         return userId
@@ -40,40 +33,47 @@ class TableViewCell: UITableViewCell {
  
     
     func config(){
-        loadIndicator.startAnimating()
+        
         userId.text = String(postData.userId)
         title.text = postData.title
         body.text = postData.body
-        networkManager.getFirstPhotoInAlbumBy(id: postData.userId) { [weak self] data in
-            DispatchQueue.main.async {
-                self?.myImageView.image = UIImage(data: data)
-                self?.loadIndicator.stopAnimating()
-            }
-        }
     }
     
     override func prepareForReuse() {
-        myImageView.image = nil
+        collectionView?.reloadData()
+        let indexPath = IndexPath(item: 0, section: 0)
+       collectionView?.scrollToItem(at: indexPath, at: .left, animated: false)
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super .init(style: style, reuseIdentifier: "Cell")
-        contentView.addSubview(myImageView)
-        contentView.addSubview(loadIndicator)
         //selectionStyle = .none
         contentView.addSubview(userId)
         contentView.addSubview(id)
         contentView.addSubview(title)
         contentView.addSubview(body)
+        createCollectionView()
     }
     
+    private func createCollectionView(){
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 10, right: 10)
+        layout.scrollDirection = .horizontal
+        
+        collectionView = UICollectionView(frame: CGRect(x: 10, y: 10, width: 10, height: 10),  collectionViewLayout: layout)
+        collectionView?.backgroundColor = nil
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView?.isPagingEnabled = true
+        contentView.addSubview(collectionView ?? UICollectionView())
+    }
     override func layoutSubviews() {
         super.layoutSubviews()
-        myImageView.translatesAutoresizingMaskIntoConstraints = false
-        loadIndicator.translatesAutoresizingMaskIntoConstraints = false
         userId.translatesAutoresizingMaskIntoConstraints = false
         title.translatesAutoresizingMaskIntoConstraints = false
         body.translatesAutoresizingMaskIntoConstraints = false
+        collectionView?.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             userId.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 5),
@@ -90,17 +90,13 @@ class TableViewCell: UITableViewCell {
             body.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor, constant: 5),
             body.widthAnchor.constraint(equalToConstant: contentView.safeAreaLayoutGuide.layoutFrame.size.width - 5)
         ])
-        NSLayoutConstraint.activate([
-            myImageView.topAnchor.constraint(equalTo: body.bottomAnchor, constant: 5),
-            myImageView.widthAnchor.constraint(equalToConstant: contentView.safeAreaLayoutGuide.layoutFrame.width - 50),
-            myImageView.heightAnchor.constraint(equalToConstant: 300),
-            myImageView.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor)
-        ])
-        NSLayoutConstraint.activate([
-            loadIndicator.centerXAnchor.constraint(equalTo: myImageView.centerXAnchor),
-            loadIndicator.centerYAnchor.constraint(equalTo: myImageView.centerYAnchor)
-        ])
-
+        
+        collectionView?.topAnchor.constraint(equalTo: body.bottomAnchor, constant: 5).isActive = true
+        collectionView?.widthAnchor.constraint(equalToConstant: contentView.safeAreaLayoutGuide.layoutFrame.width - 50).isActive = true
+        collectionView?.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        collectionView?.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    
+        
     }
     
     required init?(coder: NSCoder) {
@@ -118,4 +114,25 @@ class TableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension TableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let itemCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell {
+            itemCell.postData = postData
+            itemCell.config()
+            return itemCell
+        }
+        print("error")
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: contentView.safeAreaLayoutGuide.layoutFrame.width - 60, height: 280)
+        }
+    
 }
